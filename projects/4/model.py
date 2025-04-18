8d75d127e1782795a024099674022b7266dba6da
Max8TaganskyGang@aimasters <PinkMaxxxSS@yandex.ru>
2025-04-18 20:35:49 +0000

    pochemu kazhdyi den budto ya snimays v filme

--- projects/4/model.py
+++ projects/4/model.py
@@ -2,9 +2,9 @@ from pyspark.ml.regression import LinearRegression
 from pyspark.ml.feature import *
 from pyspark.ml import Pipeline
 
-
+tok = Tokenizer(inputCol="reviewText", outputCol="words")
 pipeline = Pipeline(stages=[
-    Tokenizer(inputCol="reviewText", outputCol="words"),
+    tok,
     HashingTF(numFeatures=50, binary=True, inputCol=tokenizer.getOutputCol(), outputCol="word_vector"),
     LinearRegression(featuresCol=hasher.getOutputCol(), labelCol="overall", maxIter=15)
 ])
