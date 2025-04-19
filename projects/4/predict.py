7d18a0ed6ecc991a0aca00a7c9d3dc2726efa7dc
Max8TaganskyGang@aimasters <PinkMaxxxSS@yandex.ru>
2025-04-19 07:00:13 +0000

    rrr

--- projects/4/predict.py
+++ projects/4/predict.py
@@ -9,10 +9,11 @@ model_path = sys.argv[1]
 test_path = sys.argv[2]
 output_path = sys.argv[3]
 
-from pyspark.ml import PipelineModel
-model = PipelineModel.load(model_path)
+#from pyspark.ml import PipelineModel
+#model = PipelineModel.load(model_path)
+from mofel import pipeline
 
 test_df = spark.read.json(test_path)
-predictions = model.transform(test_df)
+predictions = pipeline.transform(test_df)
 
 predictions.select("id", "prediction").write.mode("overwrite").csv(output_path)
